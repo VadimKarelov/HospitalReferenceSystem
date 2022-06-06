5f1d0aa1d281083cf16d376e079386b0c06d40da
VadimKarelov <vadim.karelov.02@mail.ru>
2022-06-06 21:01:30 +0500

    Change project hierarchy

---
+++ HospitalReferenceSystem/Entities/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalReferenceSystem.Entities
+{
+    internal class Patient
+    {
+        public int ID { get; set; }
+        public string FIO { get; set; }
+        public int DiagnosisID { get; set; }
+        public int WardID { get; set; }
+        public int DoctorID { get; set; }
+        public string Status { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
