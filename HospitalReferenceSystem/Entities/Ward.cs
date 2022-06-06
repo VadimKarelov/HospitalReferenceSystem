325a3ce8c9d27089a1e350563c421e995aff481a
VadimKarelov <vadim.karelov.02@mail.ru>
2022-06-06 22:10:43 +0500

    Add other enteties

---
+++ HospitalReferenceSystem/Entities/Ward.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalReferenceSystem.Entities
+{
+    internal class Ward
+    {
+        public int Number { get; set; }
+        public string Type { get; set; }
+    }
+}
