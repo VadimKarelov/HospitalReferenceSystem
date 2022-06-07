7d7dd66c609dae167004e8e13c134beb120cccd1
VadimKarelov <vadim.karelov.02@mail.ru>
2022-06-07 12:03:06 +0500

    Add example of table class

---
+++ HospitalReferenceSystem/Tables/Request.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalReferenceSystem.Tables
+{
+    internal class Request
+    {
+        public static string ConnectionString { get; set; } = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public static async Task<DataSet> RequestAsync(string sqlExpression)
+        {
+            return await new Task<DataSet>(() => DoRequest(sqlExpression));
+        }
+
+        private static DataSet DoRequest(string sqlExpression)
+        {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExpression, connection);
+                
+                adapter.Fill(ds);
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ HospitalReferenceSystem/Tables/TablePatient.cs
@@ -0,0 +1,44 @@
+﻿using HospitalReferenceSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalReferenceSystem.Tables
+{
+    internal class TablePatient
+    {
+        public List<Patient> GetAllPatients()
+        {
+            Task<DataSet> t = Request.RequestAsync("SELECT * FROM Sick");
+            t.Wait();
+            DataSet ds = t.Result;
+
+            List<Patient> res = new List<Patient>();
+
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataRow row in dt.Rows)
+                {
+                    object[] arr = row.ItemArray;
+
+                    Patient patient = new Patient()
+                    {
+                        ID = (int)row[0],
+                        FIO = (string)row[1],
+                        DiagnosisID = (int)row[2],
+                        WardID = (int)row[3],
+                        DoctorID = (int)row[4],
+                        Status = (string)row[5],
+                        Login = (string)row[6],
+                        Password = (string)row[7],
+                    };
+                }
+            }
+
+            return res;
+        }
+    }
+}
